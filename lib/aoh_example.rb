3ea76bfa13c5214fc3f4a3cae30cc4a3a6fe319b
Max M <maxmcfadden88@gmail.com>
2019-11-13 06:00:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,9 +7,9 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  result = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+[DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
-puts assembled_aoh
+
 
 
 
